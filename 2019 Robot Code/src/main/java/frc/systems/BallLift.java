b8015e4a03caa966989651db9f73bbc00c45d3a0
Jesse <1crimsonfalcon@gmail.com>
2019-01-24 18:51:21 -0800

    added BallLift.java

---
+++ 2019 Robot Code/src/main/java/frc/systems/BallLift.java
@@ -0,0 +1,70 @@
+package org.usfirst.frc.team4276.robot;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class BallLift {
+
+	Value lowPosition = DoubleSolenoid.Value.kForward;
+	Value highPosition = DoubleSolenoid.Value.kReverse;
+	double backPower = 1;
+	double frontPower = 1;
+	
+	VictorSP backroller, frontroller;
+	DoubleSolenoid diverter;
+	
+	public BallLift(int backPort, int frontPort, int divertA, int divertB) {
+		backroller = new VictorSP(backPort);
+		frontroller = new VictorSP(frontPort);
+		diverter = new DoubleSolenoid(divertA, divertB);
+		diverter.set(highPosition);
+	}
+	
+	public void performMain() {
+		
+		if(Robot.xboxJoystick.getRawButton(0)) {
+			reverse();
+		}
+		else if(Robot.xboxJoystick.getRawButton(1)){
+			lowScore();
+		}
+		else if(Robot.xboxJoystick.getRawButton(2)){
+			highScore();
+		}
+		else{
+			stop();
+		}
+	}
+	
+	public void lowScore() {
+		
+		diverter.set(lowPosition);
+		frontroller.set(frontPower);
+		backroller.set(backPower);
+		
+	}
+	
+	public void highScore() {
+		
+		diverter.set(highPosition);
+		frontroller.set(frontPower);
+		backroller.set(backPower);
+		
+	}
+	
+	public void reverse() {
+		
+		frontroller.set(-frontPower);
+		backroller.set(-backPower);
+		
+	}
+	
+	public void stop() {
+		
+		frontroller.set(0);
+		backroller.set(0);
+		
+	}
+	
+}
